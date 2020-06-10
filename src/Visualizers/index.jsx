6a3657d1b442d1c46062470e95af074a9af001df
Susmitha5c3 <lakshmisusmitha13@gmail.com>
2020-06-10 13:20:59 +0530

    remove switch

--- src/Visualizers/index.jsx
+++ src/Visualizers/index.jsx
@@ -6,13 +6,11 @@ const MainPage = () => {
     return (
         
         <body class={styles.bodystyle}>
-            {/* <div class={styles.whole}> */}
             <h1>Dijkstra's Maze Runner</h1>
                 <div class={styles.container}>
                     <button class={styles.button1}><Link class={styles.link1} to="/pathstar">Star Wars</Link></button>
                     <button class={styles.button2}> <Link class={styles.link2} to="/pathdragon">Dragon Ball</Link></button>
                 </div>
-            {/* </div> */}
         </body>
     )
 };
